781c99034297fca1e7ff79cb49e4fd29c7b475ab
Gurjap Singh <nightmare7468@gmail.com>
2025-08-14 00:57:20 +0530

    login_code

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,16 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { dark, neobrutalism } from '@clerk/themes'
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -15,31 +25,34 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable}`} >
+    <ClerkProvider appearance={{baseTheme: [dark]}}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.variable}`} >
 
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
 
-        {/* header */}
+          {/* header */}
+          <Header />
 
-        <main className="min-h-screen"> 
-          {children}
-        </main>
+          <main className="min-h-screen"> 
+            {children}
+          </main>
 
-        {/* footer */}
-        <footer className="text-center py-4">
-          <div className="container mx-auto px-4">
-            <p>&copy; {new Date().getFullYear()} BookMyDoc. All rights reserved.</p>
-          </div>
-        </footer>
+          {/* footer */}
+          <footer className="text-center py-4">
+            <div className="container mx-auto px-4">
+              <p>&copy; {new Date().getFullYear()} BookMyDoc. All rights reserved.</p>
+            </div>
+          </footer>
 
-          </ThemeProvider>
-      </body>
-    </html>
+            </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
-const header = () => {
+const Header = () => {
   return (
-    <header>
-        <h1>Book My Doctor</h1>
-    </header>
-  )
-}
+    <header className="fixed top-0 w-full border-b border-gray-800 bg-background/80 backdrop-blur-md  z-10 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/logo-single.png"
+            alt="BookMyDoc"
+            width={200}
+            height={60}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center space-x-2">
+          <SignedOut>
+           
+            <SignInButton>
+              <Button variant="secondary">
+                Sign In
+              </Button>
+            </SignInButton>
+          </SignedOut>
 
-export default header
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "h-12 w-12",
+                  userButtonPopoverCard:"shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold"
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
