26b478309dc275bcf1f20ea081f93c123d874c7d
Gurjap Singh <nightmare7468@gmail.com>
2025-09-22 22:53:50 +0530

    EX home page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { db } from "./firebase/config";
 import { collection, addDoc } from "firebase/firestore";
-import HomePage from "@/pages/homePage";
+import HomePage from "@/pages/homePageEx";
 
 const Home = () => {
   const [name, setName] = useState("");
