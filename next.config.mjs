36a31c23a8881be32bbad8a3fc8002ea216e397c
Gurjap Singh <nightmare7468@gmail.com>
2025-10-10 00:15:18 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'img.clerk.com',
+    ],
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
